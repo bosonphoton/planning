41237319da913ed5da3100639f38c957cc0a69f1
Varun Gangal <varun.gangal@patronus.ai>
2025-08-03 12:32:40 -0400

    Updating to make it possible to have different providers, for now openai the default and with the option of handling Together
    Checks that it works with Deepseek v3 0324 [which together provides]
    
    Updated README to have detailed commands for each.

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-Run with:
-python3 main.py > out.txt
+Run with openai:
 
-- msg me to get env file if u dont have the api keys
+```python
+python main.py --provider openai --model_name gpt-3.5-turbo --num_samples 200 &> output_gpt35turbo.resultlog
+```
+
+Run with together:
+
+```python
+python main.py --provider together --model_name deepseek-ai/DeepSeek-V3 --num_samples 200 &> output_together_deepseek_v3.resultlog
+```
+
+
+
+* Before you start *
+
+1. Install requirements via ```pip install -r requirements.txt```
+2. Ensure you have the system variables OPENAI_API_KEY_1, ... OPENAI_API_KEY_4 (for openai) and likewise TOGETHER_API_KEY_1, ... TOGETHER_API_KEY_4 (for together)
+
+
+
+- msg Chelsea to get env file if u dont have the api keys, or check our discord for messages by Varun and others
--- main.py
+++ main.py
@@ -1,25 +1,60 @@
 from datasets import load_dataset
 from typing import Dict
+
 from openai import OpenAI
+from together import Together
+
 import gameof24
 import os
 from dotenv import load_dotenv
 
+
+import argparse
+
+
 load_dotenv()  # Loads the .env file
 
-NUM_SAMPLES = 200
+parser = argparse.ArgumentParser(description="Financial IF Evaluation")
+parser.add_argument("--provider", default="openai",
+                       help="Model provider, either API based or litellm/vllm")
+parser.add_argument("--model_name", default="gpt-3.5-turbo",
+                       help="model name as per the provider")
+parser.add_argument("--num_samples", type=int, default=200,
+                       help="number of samples to evaluate on")
+args = parser.parse_args()
 
-API_KEYS = [
-    os.getenv("OPENAI_API_KEY_1"),
-    os.getenv("OPENAI_API_KEY_2"),
-    os.getenv("OPENAI_API_KEY_3"),
-    os.getenv("OPENAI_API_KEY_4")
-]
 
-def make_client(api_key):
-    return OpenAI(api_key=api_key)
 
-openai_clients = [make_client(k) for k in API_KEYS]
+NUM_SAMPLES = args.num_samples
+PROVIDER = args.provider
+MODEL_NAME = args.model_name
+
+API_KEYS =[]
+
+if PROVIDER == "openai":
+    API_KEYS = [
+        os.getenv("OPENAI_API_KEY_1"),
+        os.getenv("OPENAI_API_KEY_2"),
+        os.getenv("OPENAI_API_KEY_3"),
+        os.getenv("OPENAI_API_KEY_4")
+    ]
+elif PROVIDER == "together":
+    API_KEYS = [
+        os.getenv("TOGETHER_API_KEY_1"),
+        os.getenv("TOGETHER_API_KEY_2"),
+        os.getenv("TOGETHER_API_KEY_3"),
+        os.getenv("TOGETHER_API_KEY_4")
+    ]
+
+
+
+def make_client(api_key,provider):
+    if provider == "openai":
+        return OpenAI(api_key=api_key)
+    else:
+        return Together(api_key=api_key)
+
+provider_clients = [make_client(k,PROVIDER) for k in API_KEYS]
 
 # Login using e.g. `huggingface-cli login` to access this dataset
 ds = load_dataset("nlile/24-game")
@@ -49,9 +84,9 @@ for i in range(NUM_SAMPLES):
     difficulty = problem['difficulty']
     numbers = problem['numbers']
 
-    current_client = openai_clients[i % len(openai_clients)]
+    current_client = provider_clients[i % len(provider_clients)]
 
-    if gameof24.play_24game_with_llm(numbers=numbers, stats=chosen_action_count, client=current_client):
+    if gameof24.play_24game_with_llm(numbers=numbers, stats=chosen_action_count, model_name=MODEL_NAME, client=current_client):
         solved_count += 1
         difficulties_solved[difficulty] += 1
     all_difficulies[difficulty] += 1
